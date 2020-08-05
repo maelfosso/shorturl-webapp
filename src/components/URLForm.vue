1e5ee3a2c63b47505f6cd98140626edf6278ea98
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-08-05 13:20:19 +0100

    URLForm data and component set

---
+++ src/components/URLForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <form>
+      <input type="url" name="url" />
+      <button type="submit">Shorten</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  private url!: String;
+  private errors: String[] = [];
+  
+  public onSubmit(): void {
+    if (!this.url) {
+      this.errors.push('URL can not be empty!');
+    }
+
+    this.$emit('url-submitted', this.url);
+    this.url = "";
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
