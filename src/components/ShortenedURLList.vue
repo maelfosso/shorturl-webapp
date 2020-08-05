5478eddb65644f2cc9c38a95efa54294b03ae35b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-08-05 17:34:28 +0100

    ShortenedURLList component init

---
+++ src/components/ShortenedURLList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ShortenedURLList extends Vue {
+  private urls:any[] = [];
+
+  mounted() {
+    this.$on('url-submitted', (shortenedURL:any) {
+      this.urls.push(shortenedURL);
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
